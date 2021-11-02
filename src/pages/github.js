83521691042751e642213b093c2976c5e84271a7
Gap Dynamics <asadullah@gap-dynamics.com>
2021-11-02 14:43:42 +0500

    facebook commented

--- src/pages/github.js
+++ src/pages/github.js
@@ -3,22 +3,19 @@ import axios from "axios";
 import LoginGithub from "react-login-github";
 
 const onSuccess = async (response) => {
-  const data = new FormData();
-  data.append("client_id", "adc691926161c5d0f748");
-  data.append("client_secret", "6cb18ec7b1f2e685fb02151ccd5ac880f994e584");
-  data.append("code", response.code);
-  data.append("redirect_uri", "http://localhost:3001/");
-
-  // console.log("data payload", response);
+  console.log("data payload", response);
   await axios
-    .post(`https://github.com/login/oauth/access_token`, data)
+    .post(`http://192.168.18.15:3000/api/social/github/callback`, response, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    })
     .then((data) => {
       console.log("daata in respiose", data);
-      //return data;
+      return data;
     })
     .catch((err) => {
-      console.log("err message", err);
-      //return err;
+      return err;
     });
 
   /**
