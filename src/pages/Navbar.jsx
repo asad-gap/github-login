6facd2ba494c06bb61024931e5b2c470d452b04d
Gap Dynamics <asadullah@gap-dynamics.com>
2021-11-02 11:58:05 +0500

    facebook commented

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link, Route } from "react-router-dom";
 import { LinkedInPopUp } from "react-linkedin-login-oauth2";
 import LinkedInPage from "./linkedIn";
 import GitHubPage from "./github";
-import FaceBookLogin from "./facebook";
+// import FaceBookLogin from "./facebook";
 const Navbar = (props) => {
   return (
     <div>
@@ -12,7 +12,7 @@ const Navbar = (props) => {
       <Route path="/" component={LinkedInPage} />
 
       <Link to="/github" component={GitHubPage} />
-      <Link to="/facebook" component={FaceBookLogin} />
+      {/* <Link to="/facebook" component={FaceBookLogin} /> */}
       <Link to="/login">Login</Link>
     </div>
   );
